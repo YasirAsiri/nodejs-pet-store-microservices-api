ad0a4fde17d760525d391ba4a545baaf5a07724f
Yasir Asiri <iyasser89@gmail.com>
2020-04-11 06:33:23 +1000

    pet service constrains updated

--- pet-service/config/constants.js
+++ pet-service/config/constants.js
@@ -11,5 +11,10 @@ module.exports.ROUTE_NOT_FOUND = 'You are at wrong place. Shhoooo...';
 module.exports.SERVER_ERROR_MESSAGE = 'Something bad happend. It\'s not you, it\'s me.';
 
 /* Rest of constants and messages */
-module.exports.OPERATION_FAILD = 'Operation faild. '
-module.exports.PET_ADDED_SUCCESS= 'Pet was successfully added. '
+module.exports.OPERATION_FAILD = 'Operation faild. ';
+module.exports.PET_ADDED_SUCCESS= 'Pet was successfully added. ';
+module.exports.PET_ID_NOT_EXIST = 'Pet Id does not exist. ';
+module.exports.PET_UPDATED_SUCCESS = 'Pet was updated successfully. ';
+module.exports.PET_DELETED_SUCCESS = 'Pet was deleted successfully. ';
+module.exports.PET_STATUS_INVALID = 'Pet status invaild - options [available, pending, sold]';
+module.exports.NO_PETS_STORED = 'No pets records are currently stored. ';
